7a065d4e1ae3059b2f73987ec25d37ea2bb79170
furkantatlisu <furkan58tatlisu@gmail.com>
2022-10-23 14:13:50 +0300

    annotations created

--- src/test/java/deneme.java
+++ src/test/java/deneme.java
@@ -1,3 +1,4 @@
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class deneme {
@@ -5,6 +6,16 @@ public class deneme {
     @Test
     public void Demo(){
         System.out.println("hello");
+    }
+
+    @Test
+    public void Demo1(){
+        System.out.println("hello1");
+    }
+
+    @BeforeTest
+    public void prerequest(){
+        System.out.println("executed first");
     }
 
 }
