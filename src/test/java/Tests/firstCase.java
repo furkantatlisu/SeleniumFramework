a3e497a32d7300cfee1c217505f50a488ca18047
furkantatlisu <furkan58tatlisu@gmail.com>
2022-12-07 15:01:45 +0300

    fixed

--- src/test/java/Tests/firstCase.java
+++ src/test/java/Tests/firstCase.java
@@ -1,7 +1,9 @@
+package Tests;
+
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class deneme {
+public class firstCase {
 
     @Test
     public void Demo(){
